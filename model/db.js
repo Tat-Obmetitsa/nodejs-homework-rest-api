508e90c6fc2ab90c94e372292d33687c610ec11c
Tetiana Obmetytsia <twinkle.lif.love@gmail.com>
2021-06-18 20:53:43 +0300

    refactor: use mongodb

---
+++ model/db.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+const uriDB = process.env.URI_DB
+
+const db = MongoClient.connect(uriDB, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 5 })
+
+process.on('SIGINT', async () => {
+    const client = await db
+    client.close()
+    console.log('Connection to DB terminated')
+    process.exit(1)
+})
+
+module.exports = db
